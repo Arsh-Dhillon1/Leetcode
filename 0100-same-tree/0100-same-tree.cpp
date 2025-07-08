8582b1caaf372ddb48d67434ce50029f28195744
Arsh-Dhillon1 <151738122+Arsh-Dhillon1@users.noreply.github.com>
2025-07-08 09:38:24 +0530

    Time: 0 ms (100%), Space: 12.8 MB (78.09%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -11,23 +11,30 @@
  */
 class Solution {
 public:
-    void func(TreeNode* root, vector<int>& ans){
-        if(root==nullptr){
-            ans.push_back(INT_MIN);
-            return ;
-        }
-        func(root->left,ans);
-        func(root->right,ans);
-        // if(root->val==NULL){
-        //     ans.push_back(-1);
-        // }
-        ans.push_back(root->val);
-    }
+    // void func(TreeNode* root, vector<int>& ans){
+    //     if(root==nullptr){
+    //         ans.push_back(INT_MIN);
+    //         return ;
+    //     }
+    //     func(root->left,ans);
+    //     func(root->right,ans);
+    //     // if(root->val==NULL){
+    //     //     ans.push_back(-1);
+    //     // }
+    //     ans.push_back(root->val);
+    // }
     bool isSameTree(TreeNode* root1, TreeNode* root2) {
-        vector<int> ans;
-        vector<int> ans1;
-        func(root1,ans);
-        func(root2,ans1);
-        return ans==ans1;
+        // vector<int> ans;
+        // vector<int> ans1;
+        // func(root1,ans);
+        // func(root2,ans1);
+        // return ans==ans1;
+        if(root1==nullptr && root2==nullptr)return true;
+        if(root1==nullptr || root2==nullptr)return false;
+        if(root1->val!=root2->val)return false;
+        bool left=isSameTree(root1->left,root2->left);
+        bool right=isSameTree(root1->right,root2->right);
+        return left&&right;
+        
     }
 };
