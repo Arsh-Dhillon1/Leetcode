8756944cdd4ba6a73100a40dcb7fb40b23997723
Arsh-Dhillon1 <151738122+Arsh-Dhillon1@users.noreply.github.com>
2025-07-11 12:39:23 +0530

    Time: 7 ms (41.93%), Space: 24 MB (31.88%) - LeetHub

---
+++ LeetCode/Medium/0056-merge-intervals/0056-merge-intervals.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& arr) {
+        vector<vector<int>> ans;
+        if(arr.empty()) return ans;
+        sort(arr.begin(),arr.end());
+        ans.push_back(arr[0]);
+        for(int i=1;i<arr.size();i++){
+            vector<int>& last=ans.back();
+            if(last[1]>=arr[i][0]){
+                last[1]=max(last[1],arr[i][1]);
+            }
+            else{
+                ans.push_back(arr[i]);
+            }
+        }
+
+        return ans;
+    }
+};
