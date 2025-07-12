ddf228861a2ea5acc8e0871b967f022f7705731f
Arsh-Dhillon1 <151738122+Arsh-Dhillon1@users.noreply.github.com>
2025-07-12 22:15:46 +0530

    Time: 0 ms (100%), Space: 16.3 MB (64.38%) - LeetHub

---
+++ LeetCode/Medium/0061-rotate-list/0061-rotate-list.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        if(head==nullptr || head->next==nullptr || k==0) return head;
+        ListNode* tail = head;
+        int length=1;
+        while (tail->next) {
+            tail = tail->next;
+            length++;
+        }
+
+        // Step 2: Calculate effective rotations
+        k = k % length;
+        if (k == 0) return head;
+
+        ListNode* start=head;
+
+        for(int i=1;i<length-k;i++){
+            start=start->next;
+        }
+        ListNode* newstart=start->next;
+        start->next=NULL;
+        
+        tail->next=head;
+        return newstart;
+    }
+};
