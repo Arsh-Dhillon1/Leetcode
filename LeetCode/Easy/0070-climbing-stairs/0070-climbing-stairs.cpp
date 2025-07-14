d90166719170f4db49faf636c6fce8e8f8a403e5
Arsh-Dhillon1 <151738122+Arsh-Dhillon1@users.noreply.github.com>
2025-07-14 10:06:36 +0530

    Time: 0 ms (100%), Space: 7.9 MB (58.96%) - LeetHub

---
+++ LeetCode/Easy/0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if (n <= 1)
+            return 1;
+        // int len = n + 1;
+        int arr[n+1];
+        arr[0] = 1;
+        arr[1] = 1;
+        for (int i = 2; i < n+1; i++) {
+            arr[i] = arr[i - 2] + arr[i - 1];
+        }
+        return arr[n];
+    }
+};
