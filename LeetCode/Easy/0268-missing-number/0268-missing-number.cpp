8c120e936df2d5c3edb2fb27be520e2b94bb405c
Arsh-Dhillon1 <151738122+Arsh-Dhillon1@users.noreply.github.com>
2025-07-26 02:17:37 +0530

    Time: 3 ms (21.22%), Space: 21.8 MB (38.39%) - LeetHub

---
+++ LeetCode/Easy/0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& arr) {
+        for(int i=0;i<arr.size();){
+            if(arr[i]!=i && arr[i]<arr.size()){
+                swap(arr[i],arr[arr[i]]);
+            }
+            else{
+                i++;
+            }
+        }
+        for(int i=0;i<arr.size();i++){
+            if(arr[i]!=i){
+                return i;
+            }
+        }
+        return arr.size();
+    }
+};
