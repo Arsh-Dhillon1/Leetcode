6401308f180c01a0a65940a5b2699aa5d74dda28
Arsh-Dhillon1 <151738122+Arsh-Dhillon1@users.noreply.github.com>
2025-07-26 01:53:41 +0530

    Time: 3 ms (70.53%), Space: 14.8 MB (50.73%) - LeetHub

--- LeetCode/Easy/0001-two-sum/0001-two-sum.cpp
+++ LeetCode/Easy/0001-two-sum/0001-two-sum.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int>ans;
-        for(int i=0;i<nums.size();i++){
-            for(int j=i+1;j<nums.size();j++){
-                if(nums[i]+nums[j] == target){
-                    ans.push_back(i);
-                    ans.push_back(j);
-                }
+    vector<int> twoSum(vector<int>& arr, int target) {
+        unordered_map<int,int> hash;
+        for(int i=0;i<arr.size();i++){
+            int remaining=target-arr[i];
+            if(hash.count(remaining)){
+                return {hash[remaining],i};
+            }
+            else{
+                hash[arr[i]]=i;
             }
         }
-        return ans;
+        return {1,2,3,4,5};
     }
 };
