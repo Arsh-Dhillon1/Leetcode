7d290649b0aa7652f4248661f965221cb42225eb
Arsh-Dhillon1 <151738122+Arsh-Dhillon1@users.noreply.github.com>
2025-08-25 00:01:49 +0530

    Time: 0 ms (100%), Space: 15.3 MB (33.08%) - LeetHub

---
+++ LeetCode/Medium/0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    int binarySearch(vector<int>& arr, int low, int high, int target){
+        int n=arr.size();
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(arr[mid]==target){
+                return mid;
+            }
+            else if(arr[mid]<target){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        return -1;
+    }
+    int minIndex(vector<int>& arr, int low, int high){
+        int n=arr.size();
+        int index=-1;
+        if (arr[low] <= arr[high]) return low; // already sorted
+        while(low<=high){
+            int mid=(low+high)/2;
+            int prev=(mid+n-1)%n;
+            int next=(mid+1)%n;
+            if(arr[mid]<=arr[prev] && arr[mid]<=arr[next]){
+                return mid;
+            }
+            else if(arr[0]<=arr[mid]){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        return index;
+    }
+
+    int search(vector<int>& arr, int target) {
+        int n=arr.size();
+        int index=minIndex(arr,0,n-1);
+        int first=binarySearch(arr,0,index-1,target);
+        int second=binarySearch(arr,index,n-1,target);
+        if(first==-1 && second==-1){
+            return -1;
+        }
+        else if(first>=0){
+            return first;
+        }
+        else{
+            return second;
+        }
+        
+    }
+};
