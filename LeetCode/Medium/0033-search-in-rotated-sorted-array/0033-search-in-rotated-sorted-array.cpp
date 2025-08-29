84d7b0caea424b29a12894caf53cc482da9b0887
Arsh-Dhillon1 <151738122+Arsh-Dhillon1@users.noreply.github.com>
2025-08-30 03:11:19 +0530

    Time: 0 ms (100%), Space: 15.2 MB (66.12%) - LeetHub

--- LeetCode/Medium/0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ LeetCode/Medium/0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,56 +1,45 @@
 class Solution {
 public:
-    int binarySearch(vector<int>& arr, int low, int high, int target){
-        int n=arr.size();
-        while(low<=high){
-            int mid=(low+high)/2;
-            if(arr[mid]==target){
+    int indexOfSmallest(vector<int>& arr) {
+        int low = 0;
+        int n = arr.size();
+        int high = n - 1;
+        if (arr[low] <= arr[high])
+            return low;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int next = (mid + 1) % n;
+            int prev = (mid + n - 1) % n;
+            if (arr[mid] < arr[next] && arr[mid] < arr[prev]) {
                 return mid;
+            } else if (arr[mid] >= arr[0]) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
             }
-            else if(arr[mid]<target){
-                low=mid+1;
-            }
-            else{
-                high=mid-1;
+        }
+        return 0;
+    }
+    int binarySearch(vector<int>& arr, int target, int low, int high) {
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
             }
         }
         return -1;
     }
-    int minIndex(vector<int>& arr, int low, int high){
-        int n=arr.size();
-        int index=-1;
-        if (arr[low] <= arr[high]) return low;
-        while(low<=high){
-            int mid=(low+high)/2;
-            int prev=(mid+n-1)%n;
-            int next=(mid+1)%n;
-            if(arr[mid]<=arr[prev] && arr[mid]<=arr[next]){
-                return mid;
-            }
-            else if(arr[0]<=arr[mid]){
-                low=mid+1;
-            }
-            else{
-                high=mid-1;
-            }
-        }
-        return index;
-    }
-
     int search(vector<int>& arr, int target) {
-        int n=arr.size();
-        int index=minIndex(arr,0,n-1);
-        int first=binarySearch(arr,0,index-1,target);
-        int second=binarySearch(arr,index,n-1,target);
-        if(first==-1 && second==-1){
-            return -1;
-        }
-        else if(first>=0){
+        int n = arr.size();
+        int min = indexOfSmallest(arr);
+        int first = binarySearch(arr, target, 0, min - 1);
+        int second = binarySearch(arr, target, min, n - 1);
+        if (first != -1)
             return first;
-        }
-        else{
-            return second;
-        }
-        
+        return second;
     }
 };
