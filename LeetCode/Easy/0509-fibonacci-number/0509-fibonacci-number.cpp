5338a51c995713299409170a5c2cf07909362ad1
Arsh-Dhillon1 <151738122+Arsh-Dhillon1@users.noreply.github.com>
2026-02-16 11:28:09 +0530

    Time: 14 ms (23%), Space: 7.6 MB (87.18%) - LeetHub

--- LeetCode/Easy/0509-fibonacci-number/0509-fibonacci-number.cpp
+++ LeetCode/Easy/0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,16 +1,8 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n<=1) return n;
-        int length=n+1;
-        int arr[length];
-        arr[0]=0;
-        arr[1]=1;
-        // int sum=0;
-
-        for(int i=2;i<length;i++){
-            arr[i]=(arr[i-1]+arr[i-2]);
-        }
-        return arr[n];
+        if(n==0) return 0;
+        if(n==1) return 1;
+        return fib(n-1)+fib(n-2);
     }
 };
